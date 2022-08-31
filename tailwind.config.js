be12cf10dc2395e8e89d5c9fd69b660a0a479f50
rafitajaen <rafitajaen@gmail.com>
2022-08-31 14:58:53 +0200

    Add Custom Colors and Small CSS Bugfixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "surface-overlay": "#1f2d40",
+        "surface-back": "#17212f",
+        "surface-card": "#1f2d40",
+        "surface-card-hover": "#304562",
+        "surface-primary": "#BA68C8",
+        "surface-accent": "#fa2862",
+      },
+    },
   },
   plugins: [],
 };
