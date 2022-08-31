c897f8eb427576bb281816bef5ff68a98f7bd0f2
rafitajaen <rafitajaen@gmail.com>
2022-08-31 20:46:56 +0200

    Import popperjs package and tooltip functionality to password-input

--- src/app/sections/hero/components/generator/password-input/password-input.component.ts
+++ src/app/sections/hero/components/generator/password-input/password-input.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { createPopper, Instance } from '@popperjs/core'
+
+// https://stackoverflow.com/questions/71338835/how-to-use-popperjs-2-with-angular-13
 
 @Component({
   selector: 'app-password-input',
@@ -7,17 +10,63 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class PasswordInputComponent implements OnInit {
 
-  @Input() password: string;
+  popperInstance!: Instance;
 
+  @Input() password: string;
   @Output() regenerateEvent = new EventEmitter<string>();
 
-  constructor() { this.password = '' }
+  @ViewChild('bt', { static: true }) button!: ElementRef
+  @ViewChild('tooltip', { static: true }) tooltip!: ElementRef
 
+  constructor() { this.password = '' }
   ngOnInit(): void {
+    this.popperInstance = createPopper(this.button.nativeElement, this.tooltip.nativeElement, {
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
+        },
+      ],
+    });
   }
 
   onRegenerate() {
     this.regenerateEvent.emit(this.password);
+  }
+
+  show(): void {
+    // Make the tooltip visible
+    this.tooltip.nativeElement.setAttribute('data-show', '');
+
+    // Enable the event listeners
+    this.popperInstance.setOptions((options: any) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: true },
+      ],
+    }));
+
+    // Update its position
+    this.popperInstance.update();
+
+    setTimeout(() => this.hide(), 1000);
+  }
+
+  hide() {
+    // Hide the tooltip
+    this.tooltip.nativeElement.removeAttribute('data-show');
+
+    // Disable the event listeners
+    this.popperInstance.setOptions((options: any) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: false },
+      ],
+    }));
   }
 
 }
